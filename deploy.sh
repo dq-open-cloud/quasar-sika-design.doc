1cb9ff9dc26eca9b5306f1bac1e04797c33317aa
dq-open-cloud <466608943@qq.com>
2021-01-05 15:24:24 +0800

    Update documents.

--- deploy.sh
+++ deploy.sh
@@ -1,54 +1,15 @@
-#!/usr/bin/env sh
-
-# 确保脚本抛出遇到的错误
 set -e
-
-initDist(){
-  echo $1 > base.js
-  npm run build
-  cd docs/.vuepress/dist
-}
-
-initDist "module.exports = '/vuepress-theme-vdoing-doc/'"
-echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
-
-# deploy to github
+npm install
+git add .
+git commit -m 'Update documents.'
+git push
+npm run build
+cd src/.vuepress/dist
 echo 'quasar.doc.sikacode.com' > CNAME  # 自定义域名
-#if [ -z "$GITHUB_TOKEN" ]; then
-#  msg='deploy'
-#  githubUrl=git@github.com:dq-open-cloud/quasar-sika-design-doc.git
-#else
-#  msg='来自github actions的自动部署'
-#  githubUrl=https://xugaoyi:${GITHUB_TOKEN}@git@github.com:dq-open-cloud/quasar-sika-design-doc.git
-#  git config --global user.name "dq-open-cloud"
-#  git config --global user.email "466608943@qq.com"
-#fi
-#
-#initGit(){
-#  git init
-#  git add -A
-#  git commit -m "${msg}"
-#}
-#initGit
-#git push -f $githubUrl master:gh-pages # 推送到github
-#
-#cd -
-#rm -rf docs/.vuepress/dist
-#
-#
-## deploy to coding
-#initDist "module.exports = '/'"
-#echo 'quasar.doc.sikacode.com' > CNAME  # 自定义域名
-#echo 'google.com, pub-7828333725993554, DIRECT, f08c47fec0942fa0' > ads.txt # 谷歌广告相关文件
-#
-#if [ -z "$CODING_TOKEN" ]; then  # -z 字符串 长度为0则为true
-#  codingUrl=git@e.coding.net:xgy/vdoing-doc/vdoing-doc.git
-#else
-#  codingUrl=https://HmuzsGrGQX:${CODING_TOKEN}@e.coding.net/xgy/vdoing-doc/vdoing-doc.git
-#fi
-#
-#initGit
-#git push -f $codingUrl master # 推送到coding
-
+git init -b main
+git config user.name 'dq-open-cloud'
+git config user.email '466608943@qq.com'
+git add -A
+git commit -m "deploy: $(TZ="Asia/Shanghai" date +"%Y-%m-%d %T %Z")"
+git push -f git@github.com:dq-open-cloud/quasar-sika-design-doc.git main:gh-pages
 cd -
-rm -rf docs/.vuepress/dist
